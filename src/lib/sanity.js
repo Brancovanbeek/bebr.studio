b94fa9b96427f7bcde26073fdcc71982aa2c6b9f
Brancovanbeek <branco.van.beek@hva.nl>
2026-02-16 15:37:57 +0100

    fix: remove hardcoded id's, #27

--- src/lib/sanity.js
+++ src/lib/sanity.js
@@ -4,6 +4,6 @@ import { createClient } from '@sanity/client'
 export const client = createClient({
   projectId: import.meta.env.PUBLIC_SANITY_PROJECT_ID,
   dataset: import.meta.env.PUBLIC_SANITY_DATASET,
-  apiVersion: '2024-01-01',
+  apiVersion: import.meta.env.PUBLIC_SANITY_API_VERSION,
   useCdn: true,
 })

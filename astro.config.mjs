2e5a8f891da4b7df4991207f8207634adc1071a3
Brancovanbeek <branco.van.beek@hva.nl>
2026-02-17 14:43:26 +0100

    fix: merge conflicts

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,8 @@ import { defineConfig } from 'astro/config';
 import sanity from '@sanity/astro';
 import react from '@astrojs/react';
 import svelte from '@astrojs/svelte';
-<<<<<<< feature/hero-section
 import vercel from '@astrojs/vercel/serverless';
-=======
 import sanityImg from "@otterstack/sanity-img-astro/integration";
->>>>>>> dev
 
 export default defineConfig({
   output: 'static',
